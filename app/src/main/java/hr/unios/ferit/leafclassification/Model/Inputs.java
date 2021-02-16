d6b389b2eda1f6808c5af91cf7cc5d69c1ea8ea3
Dominik <dominik.tkalcec@gdi.net>
2021-02-16 12:59:40 +0100

    Deleted unused models

--- app/src/main/java/hr/unios/ferit/leafclassification/Model/Inputs.java
+++
@@ -1,7 +0,0 @@
-package hr.unios.ferit.leafclassification.Model;
-
-import java.util.List;
-
-public class Inputs {
-    public List<Input> input;
-}
--- app/src/main/java/hr/unios/ferit/leafclassification/Model/Sample.java
+++
@@ -1,6 +0,0 @@
-package hr.unios.ferit.leafclassification.Model;
-
-public class Sample {
-    public Inputs inputs;
-    public GlobalParameters globalParameters;
-}
